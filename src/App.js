dda6706c2e19d9bd5db2f1ea2768fdf6b74a40c8
Gianluca Zuccarelli <zissou@Zissou.local>
2019-10-28 13:33:39 +0000

    Add navbar and Bulma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Navbar from './components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo***REMOVED*** className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 ***REMOVED***
---
+++ src/components/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <section className="hero is-info">
+      <div className="hero-head">
+        <nav className="navbar main-navbar">
+          <div className="container" >
+            <div className="navbar-brand">
+              <a href="/" className="navbar-item">
+                <figure className="image is-24x24">
+                  {/* Source a placeholder */***REMOVED***
+                  {/* <img src="/images/logo.png" alt="Placeholder" /> */***REMOVED***
+                </figure>
+                &nbsp;&nbsp;
+                Resplash
+               </a>
+              <span className="navbar-burger burger">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div className="navbar-menu">
+              <div className="navbar-end">
+                <a href="/dashboard" className="navbar-item">
+                  Dashboard
+                </a>
+                <a href="/about" className="navbar-item">
+                  About
+                </a>
+                <a href="/login" className="navbar-item">
+                  Login
+                </a>
+                <a href="/signup" className="navbar-item">
+                  Signup
+                </a>
+                <div className="navbar-item has-dropdown">
+                  <a href="#" className="navbar-link">
+                    <figure className="image is-24x24">
+                      <img src="https://bulma.io/images/placeholders/32x32.png" style={{ borderRadius: 999 + 'px' ***REMOVED******REMOVED*** alt="Placeholder" />
+                    </figure>
+                    <span>&nbsp;&nbsp;@</span>
+                  </a>
+                  <div className="navbar-dropdown is-right">
+                    <hr className="navbar-divider" />
+                    <a className="navbar-item" href="/logout">
+                      Logout
+                  </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <nav className="navbar has-shadow has-text-white" style={{ background: '#3AB6FF' ***REMOVED******REMOVED***>
+        <div className="container">
+
+        </div>
+      </nav>
+    </section >
+  )
+***REMOVED***
